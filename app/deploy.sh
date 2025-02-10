185259f770c8d0b13639f94b9fe349edef7b6577
e-sugiuea <e-sugiuea@wwwave.jp>
2025-02-10 21:52:18 +0900

    デプロイ円滑化

--- app/deploy.sh
+++ app/deploy.sh
@@ -4,6 +4,9 @@ cd "$(dirname "$0")"
 rm -rf dist
 mkdir dist
 
+# build
+docker compose -f "../compose.yaml" run --rm node bash -c "npm run postinstall && npm run generate"
+
 # build結果をコピー
 cp -r .output/public/ dist
 
